bb2269c1c2e4aa620cd7cfe247fd2921713e1146
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-09-07 19:03:40 -0700

    added data obj

---
+++ static_Data/jayAboutData.js
@@ -0,0 +1,56 @@
+
+
+const aboutMeData = {
+  summary: "I am a creative, energetic and self-motivated professional in product management with experience in government, startups and corporate organizations. My experience and skill sets are heavily intertwined with my passion for tech. New technologies are reshaping the way we interact and see the world on a daily basis. I am very fortunate to live in the Bay Area to witness and participate in the technological changes. I am well versed and experienced in web product development, email marketing and digital advertising. My past and current roles have allowed me to build everything from green technology models, e-filing systems, and email marketing automation system to new ad products. I consider myself a unique hybrid between a developer, marketer and a product manager. I have the heart of a marketer with the perspective of a product manager and skills of a full stack developer. Let’s connect and swap ideas if you are looking for a product manager who thinks with a different perspective.",
+
+  skills: ["Product Manager", "Full-Stack Web Development", "Digital Advertising", "Marketing", "Digital Media", "B2C", "Data Analytics", "Monetization and Growth", "Developing and Implementing Scalable Business Processes"],
+
+  techSkills: ["Javascript", "HTML", "CSS", "JQuery", "Bootstrap", "Firebase", "Node.js", "MySQL", "MongoDB", "Express.js", "GraphQL.js", "Handlebars.js", "React.js", "Redux", "Heroku", "Wordpress"],
+
+
+  personality: [
+    {
+      title: "Life Long Learner",
+      tagline: "Curious",
+      summary: "I am very curious.I believe in the philosophy of continuous growth and development.I love to learn.I actively take classes in a variety of subjects from programming, music to yoga and much more."
+    },
+    {
+      title: "Adventure Seeker",
+      tagline: "Energetic",
+      summary: "I am a very energetic person.I am always in search of my next adventure.I am not afraid to take risks.People usually tend to feed off of this energy as a result."
+    },
+    {
+      title: "Problem Solver",
+      tagline: "Energetic",
+      summary: "My motto is there is nothing you can’t do if you set your mind to it. I like to break things apart and understand how it ticks then precede to assemble it back together with improvements. It is like a puzzle I must solve."
+    },
+    {
+      title: "Artist Creator",
+      tagline: "Creative",
+      summary: "My motto is there is nothing you can’t do if you set your mind to it. I like to break things apart and understand how it ticks then precede to assemble it back together with improvements. It is like a puzzle I must solve."
+    }
+  ],
+  
+  productDefined: [
+    {
+      title: "Marketer",
+      quote: "As a marketer, the main focus is to get the right message to the right people at the right time."
+    },
+    {
+      title: "Product Manager",
+      quote: "As a Product Manager, the main focus is to create products that address a want, a need, and a desire with usability and innovation in mind."
+    },
+    {
+      title: "Product Manager",
+      quote: "The main focus for both a marketer and product manager in the end is the same, which are the end users. Therefore, I approach any product development with both of these 2 focuses in mind."
+    }]
+
+
+
+
+
+
+}
+
+
+
