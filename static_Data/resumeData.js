6b5e7c9b9f77724477be40b7dc1f0f5b86cdcb14
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-09-13 14:39:09 -0700

    fixed resume route

--- static_Data/resumeData.js
+++ static_Data/resumeData.js
@@ -2,30 +2,31 @@ const resumeDataObj = [
   {
     company: "CBS INTERACTIVE", 
     dateRange: "April 2016 - Present",
-    jobTite: "Technical Product Manager",
-    jobDescription: [
+    title: "Technical Product Manager",
+    description: [
       "Iterate and monitor user flow experiments of internal API endpoint in order to improve first - party data collection in preparation of removal of cookies for retargeting purposes.", "Build an internal CMS to templatize ad deliver format in order to decrease operational cost & technical debt with a reusable component structure", "Oversaw the technical implementation of third-party vendor integration with prebid.js(open source library for header bidding) and SDKs for app", "Optimized and created first-party DFP creative templates in order to maximize site render performance.", "Lead the data reform of the alignment between page & ad performance analytics", "Drive KPI modeling, product roadmap, data analysis, and A/B testing to explore new market and growth opportunities", "Define new monetization product requirements for MVPs and new,features/enhancements for platform such as AMP, Apple News, OTT, & App", "Increase revenue by 2x and user engagement by 5x with redesign of CBS.com, and CBS News by optimizing user flow, product strategy, design, and technicalmcode implementation", "Monitor and facilitate the technical execution of multi-million-dollar advertising sponsorships for major events, such as the Grammy Awards"
-    ]
+    ],
+    current: true
 
   },
   {
     company: "CBS INTERACTIVE",
     dateRange: "March 2014 - March 2016",
-    jobTite: "Associate Product Manager",
-    jobDescription: ["Determined the technical feasibility of market requirements for revenue products", "Developed audience segmentation products related to behavioral, demographic,and contextual targeting and retargeting across CBS Interactive brands"]
+    title: "Associate Product Manager",
+    description: ["Determined the technical feasibility of market requirements for revenue products", "Developed audience segmentation products related to behavioral, demographic,and contextual targeting and retargeting across CBS Interactive brands"]
   },
 
   {
     company: "FORMS DIRECT",
     companyDetails: "An Immigration Technology Company)",
     dateRange: "October 2012 - March 2014",
-    jobTite: "Product Manager and Email Marketer",
-    jobDescription: ["Transformed email strategy by creating nurturing campaigns that accounted for a 10x revenue lift(~$1.8M)", "Scoped and built email customer relations marketing system thru API integration of partner vendor platform", "Ensured the development, production, testing and delivery of email campaigns and landing pages to effectively maximize ROI", "Provided requirements, wireframes and ad hoc data analysis on core desktop and mobile for citizenship product", "Tracked and reported email marketing metrics to effectively improveperformance through A/B testing"]
+    title: "Product Manager and Email Marketer",
+    description: ["Transformed email strategy by creating nurturing campaigns that accounted for a 10x revenue lift(~$1.8M)", "Scoped and built email customer relations marketing system thru API integration of partner vendor platform", "Ensured the development, production, testing and delivery of email campaigns and landing pages to effectively maximize ROI", "Provided requirements, wireframes and ad hoc data analysis on core desktop and mobile for citizenship product", "Tracked and reported email marketing metrics to effectively improveperformance through A/B testing"]
   },
  {
    company: "ASIA AMERICA MULTITECHNOLOGY ASSOCIATION",
    dateRange: "May 2012 - May 2014",
-   jobTite: "Alliance and Online Marketing Manager",
-   jobDescription: ["Enhance AAMA’s overall online presence through social media with the use of LinkedIn", "Created and managed email campaigns and business strategies to drive conference and membership sale"]
+   title: "Alliance and Online Marketing Manager",
+   description: ["Enhance AAMA’s overall online presence through social media with the use of LinkedIn", "Created and managed email campaigns and business strategies to drive conference and membership sale"]
   }
 ]
