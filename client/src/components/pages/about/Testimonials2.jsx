41dcf67b96929162327f6caa24574158b00624ce
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-09-26 20:04:26 -0700

    fixed testminoial

--- client/src/components/pages/about/Testimonials2.jsx
+++ client/src/components/pages/about/Testimonials2.jsx
@@ -54,7 +54,6 @@ const Testimonials = () => {
       <div className="owl-carousel testimonials-carousel">
         <MDBCard className="my-5 px-5 pb-5 text-center">
 
-
           <MDBCarousel
             activeItem={1}
             length={3}
@@ -63,114 +62,168 @@ const Testimonials = () => {
             multiItem
             testimonial
           >
+
             <MDBCarouselInner>
               <MDBCarouselItem itemId="1">
-                <MDBCardBody>
-          <MDBRow>
-            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              <img
-                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg"
-                className="rounded-circle z-depth-1 img-fluid"
-                alt="Sample avatar"
-              />
-              <h5 className="font-weight-bold mt-4 mb-3">Anna Williams</h5>
-              <p className="text-uppercase blue-text">Graphic designer</p>
-              <p className="grey-text">
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-                consectetur, adipisci sed quia non numquam modi tempora eius.
-              </p>
-              <ul className="list-unstyled mb-0">
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="facebook-f" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="twitter" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="instagram" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
+                <MDBRow>
 
-            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              <img
-                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(3).jpg"
-                className="rounded-circle z-depth-1 img-fluid"
-                alt="Sample avatar"
-              />
-              <h5 className="font-weight-bold mt-4 mb-3">John Doe</h5>
-              <p className="text-uppercase blue-text">Web Developer</p>
-              <p className="grey-text">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                ipsa accusantium doloremque rem laudantium totam aperiam.
-              </p>
-              <ul className="list-unstyled mb-0">
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="facebook-f" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="instagram" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
+                  <MDBCol lg="4" md="5" className="mb-lg-0 mb-5 col-lg-4 col-md-4">
+                    <img
+                      src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg"
+                      className="rounded-circle z-depth-1 img-fluid"
+                      alt="Sample avatar"
+                    />
+                    <h5 className="font-weight-bold mt-4 mb-3">Anna Williams</h5>
+                    <p className="text-uppercase blue-text">Graphic designer</p>
+                    <p className="grey-text">
+                      Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+                        consectetur, adipisci sed quia non numquam modi tempora eius.</p>
+                    <ul className="list-unstyled mb-0">
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="facebook-f" className="blue-text" />
+                      </a>
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="twitter" className="blue-text" />
+                      </a>
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="instagram" className="blue-text" />
+                      </a>
+                    </ul>
+                  </MDBCol>
 
-            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              <img
-                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
-                className="rounded-circle z-depth-1 img-fluid"
-                alt="Sample avatar"
-              />
-              <h5 className="font-weight-bold mt-4 mb-3">Maria Smith</h5>
-              <p className="text-uppercase blue-text">Photographer</p>
-              <p className="grey-text">
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                qui officia deserunt mollit anim est fugiat nulla id eu laborum.
-              </p>
-              <ul className="list-unstyled mb-0">
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="facebook-f" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="instagram" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="dribbble" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
-           
-                  
-                        
-                          
-            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              <img
-                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(32).jpg"
-                className="rounded-circle z-depth-1 img-fluid"
-                alt="Sample avatar"
-              />
-              <h5 className="font-weight-bold mt-4 mb-3">Tom Adams</h5>
-              <p className="text-uppercase blue-text">Backend Developer</p>
-              <p className="grey-text">
-                Perspiciatis repellendus ad odit consequuntur, eveniet earum
-                nisi qui consectetur totam officia voluptates perferendis
-                voluptatibus aut.
-              </p>
-              <ul className="list-unstyled mb-0">
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="facebook-f" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="github" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
-          </MDBRow>
-        </MDBCardBody>
-        </MDBCarouselItem>
+                  {/* card sample 2 */}
+                  <MDBCol lg="4" md="5" className="mb-lg-0 mb-5 col-lg-4 col-md-4">
+                    <img
+                      src="https://mdbootstrap.com/img/Photos/Avatars/img%20(3).jpg"
+                      className="rounded-circle z-depth-1 img-fluid"
+                      alt="Sample avatar"
+                    />
+                    <h5 className="font-weight-bold mt-4 mb-3">John Doe</h5>
+                    <p className="text-uppercase blue-text">Web Developer</p>
+                    <p className="grey-text">
+                      Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                      ipsa accusantium doloremque rem laudantium totam aperiam</p>
+                    <ul className="list-unstyled mb-0">
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="facebook-f" className="blue-text" />
+                      </a>
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="instagram" className="blue-text" />
+                      </a>
+                    </ul>
+                  </MDBCol>
+
+                  {/* card sample 3 */}
+                  <MDBCol lg="4" md="5" className="mb-lg-0 mb-5 col-lg-4 col-md-4">
+                    <img
+                      src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
+                      className="rounded-circle z-depth-1 img-fluid"
+                      alt="Sample avatar"
+                    />
+                    <h5 className="font-weight-bold mt-4 mb-3">Maria Smith</h5>
+                    <p className="text-uppercase blue-text">Photographer</p>
+                    <p className="grey-text">
+                      Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                      qui officia deserunt mollit anim est fugiat nulla id eu laborum.</p>
+                    <ul className="list-unstyled mb-0">
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="facebook-f" className="blue-text" />
+                      </a>
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="instagram" className="blue-text" />
+                      </a>
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="dribbble" className="blue-text" />
+                      </a>
+                    </ul>
+                  </MDBCol>
+
+                </MDBRow>
+              </MDBCarouselItem>
+              {/* end of slide 1 */}
+
+              <MDBCarouselItem itemId="2">
+                <MDBRow>
+
+                  <MDBCol lg="4" md="5" className="mb-lg-0 mb-5 col-lg-4 col-md-4">
+                    <img
+                      src={climb}
+                      className="rounded-circle z-depth-1 img-fluid resizeImage"
+                      alt="Sample avatar"
+                    />
+                    <h5 className="font-weight-bold mt-4 mb-3">Anna Williams</h5>
+                    <p className="text-uppercase blue-text">Graphic designer</p>
+                    <p className="grey-text">
+                      Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+                        consectetur, adipisci sed quia non numquam modi tempora eius.</p>
+                    <ul className="list-unstyled mb-0">
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="facebook-f" className="blue-text" />
+                      </a>
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="twitter" className="blue-text" />
+                      </a>
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="instagram" className="blue-text" />
+                      </a>
+                    </ul>
+                  </MDBCol>
+
+                  {/* card sample 2 */}
+                  <MDBCol lg="4" md="5" className="mb-lg-0 mb-5 col-lg-4 col-md-4">
+                    <img
+                      src="https://mdbootstrap.com/img/Photos/Avatars/img%20(3).jpg"
+                      className="rounded-circle z-depth-1 img-fluid"
+                      alt="Sample avatar"
+                    />
+                    <h5 className="font-weight-bold mt-4 mb-3">John Doe</h5>
+                    <p className="text-uppercase blue-text">Web Developer</p>
+                    <p className="grey-text">
+                      Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                      ipsa accusantium doloremque rem laudantium totam aperiam</p>
+                    <ul className="list-unstyled mb-0">
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="facebook-f" className="blue-text" />
+                      </a>
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="instagram" className="blue-text" />
+                      </a>
+                    </ul>
+                  </MDBCol>
+
+                  {/* card sample 3 */}
+                  <MDBCol lg="4" md="5" className="mb-lg-0 mb-5 col-lg-4 col-md-4">
+                    <img
+                      src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
+                      className="rounded-circle z-depth-1 img-fluid"
+                      alt="Sample avatar"
+                    />
+                    <h5 className="font-weight-bold mt-4 mb-3">Maria Smith</h5>
+                    <p className="text-uppercase blue-text">Photographer</p>
+                    <p className="grey-text">
+                      Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                      qui officia deserunt mollit anim est fugiat nulla id eu laborum.</p>
+                    <ul className="list-unstyled mb-0">
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="facebook-f" className="blue-text" />
+                      </a>
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="instagram" className="blue-text" />
+                      </a>
+                      <a href="#!" className="p-2 fa-lg">
+                        <MDBIcon fab icon="dribbble" className="blue-text" />
+                      </a>
+                    </ul>
+                  </MDBCol>
+
+                </MDBRow>
+              </MDBCarouselItem>
+              {/* end of slide 2 */}
+
             </MDBCarouselInner>
           </MDBCarousel>
-        </MDBCard>  
-        
+        </MDBCard>
+
       </div>
 
     </div >
